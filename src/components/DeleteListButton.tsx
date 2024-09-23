a9127654c6cd3cc0b553e54c95fb75d6161a3c0d
michaellgans <michaellgans@Victoria>
2024-09-23 16:48:03 -0500

    MLG - 4:47 PM - corrects typo on DeleteListButton

---
+++ src/components/DeleteListButton.tsx
@@ -0,0 +1,27 @@
+// DeleteListButton Component
+
+// Asset Imports
+
+// Returns a DeleteListButton Component
+export function DeleteListButton() {
+  // Define Hook
+  return (
+    // onclick="alert('Delete list')"
+    <button className="h-[30px]">
+      <svg
+        className="hidden h-[30px] w-[30px] cursor-pointer group-hover/list:block"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="{1.5}"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+    </button>
+  );
+}

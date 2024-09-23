81114ead1db99e4bdf1a724f975aa1c430ae66a7
michaellgans <michaellgans@Victoria>
2024-09-23 16:18:17 -0500

    MLG - 4:18 PM - writes a snapshot test for Footer

---
+++ src/__tests__/Footer.test.tsx
@@ -0,0 +1,15 @@
+// Test for Footer Component
+
+// Imports
+import { render } from "@testing-library/react";
+import { expect, test, it } from 'vitest';
+import { Footer } from "../components/Footer";
+
+it('Controll Test', () => {
+    expect(2 + 2).toBe(4);
+});
+
+test('Footer renders correctly', () => {
+    const { container } = render(<Footer />);
+    expect(container).toMatchSnapshot();
+})

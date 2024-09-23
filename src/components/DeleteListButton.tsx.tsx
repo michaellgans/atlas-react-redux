505978808caaa636878714205bbbf0ccbf2acc8c
michaellgans <michaellgans@Victoria>
2024-09-23 12:53:54 -0500

    MLG - 12:53 PM - Creates DeleteList Button Componnet

---
+++ src/components/DeleteListButton.tsx.tsx
@@ -0,0 +1,18 @@
+// DeleteListButton.tsx Component
+
+// Asset Imports
+
+// Script Imports
+
+// Types
+type DeleteListButton.tsxProps = {
+    prop: type;
+    prop: type;
+};
+
+// Returns a DeleteListButton.tsx Component
+export function DeleteListButton.tsx({}: DeleteListButton.tsxProps) {
+    // Define Hook
+    return (
+    );
+}

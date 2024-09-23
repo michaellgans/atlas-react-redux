28d53fa460378dcd19f254cee2b9ee56f0660285
michaellgans <michaellgans@Victoria>
2024-09-23 12:38:18 -0500

    MLG - 12:38 PM - creates Footer and imports into App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,13 @@
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
+      <Footer />
     </Provider>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+// Footer Component
+
+// Returns a Footer Component
+export function Footer() {
+  // Define Hook
+  return (
+    <footer className="sticky bottom-0 left-0 flex w-screen items-center justify-center space-x-8 border-t-2 border-blue bg-off-white-light p-8">
+      {/* onSubmit=alert('Create list') */}
+      <form>
+        <input
+          type="text"
+          placeholder="List title"
+          name="title"
+          className="border-0 bg-transparent text-3xl font-semibold text-blue placeholder:text-blue placeholder:opacity-50 focus:outline-none"
+        />
+        <button
+          type="submit"
+          className="rounded bg-teal px-6 py-4 text-xl font-semibold text-off-white-light"
+        >
+          Save
+        </button>
+        {/* onclick="alert('Clear board')" */}
+        <button
+          type="button"
+          className="rounded bg-teal px-6 py-4 text-xl font-semibold text-off-white-light"
+        >
+          Clear Board
+        </button>
+      </form>
+    </footer>
+  );
+}

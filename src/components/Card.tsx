00c7eb7733940711002ddd359d506c3df0f897c7
michaellgans <michaellgans@Victoria>
2024-09-25 13:38:47 -0500

    MLG - 1:38 PM - sets up droppable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,8 @@
 // Asset Imports
 import { DeleteCardButton } from "./DeleteCardButton";
 
-// Script Imports
+// Drag and Drop
+import { useDraggable } from "@dnd-kit/core";
 
 // Types
 type CardProps = {
@@ -14,9 +15,18 @@ type CardProps = {
 
 // Returns a Card Component
 export function Card({ title, description, cardID }: CardProps) {
-  // Define Hook
+  // Drag and Drop
+  const { attributes, listeners, setNodeRef } = useDraggable({
+    id: cardID,
+  });
+
   return (
-    <div className="card group/card m-3 flex min-h-24 w-full flex-col items-start rounded bg-off-white-light px-4 py-2 text-blue">
+    <div
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+      className="card group/card m-3 flex min-h-24 w-full flex-col items-start rounded bg-off-white-light px-4 py-2 text-blue"
+    >
       <h5 className="my-2 flex w-full items-end justify-between text-xl font-black">
         <span>{title}</span>
         {/* DELETE CARD BUTTON */}

8d4c0902afbdeca55e32f62af10b7cfef67da6ca
michaellgans <michaellgans@Victoria>
2024-09-25 21:10:53 -0500

    MLG - 9:10 PM - Drag and Drop transition added

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,16 +6,40 @@ import { List } from "./List";
 import { RootState } from "../store";
 
 // Drag and Drop
-import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import {
+  DndContext,
+  DragEndEvent,
+  DragStartEvent,
+  DragOverlay,
+} from "@dnd-kit/core";
 import { moveCard } from "../slices/cardsSlice";
+import { useState } from "react";
+import { Card } from "./Card";
 
 // Returns a Board Component
 export function Board() {
   // Define Hook
   const lists = useAppSelector((state: RootState) => state.listsSlice.items);
+  const cards = useAppSelector((state: RootState) => state.cards.items);
   const dispatch = useAppDispatch();
 
+  const [activeCard, setActiveCard] = useState<null | {
+    title: string;
+    description: string;
+    id: string;
+  }>(null);
+
   // Drag and Drop
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event;
+
+    const activeCardData = cards.find((card) => card.id === active.id);
+
+    if (activeCardData) {
+      setActiveCard(activeCardData);
+    }
+  }
+
   function handleDragEnd(event: DragEndEvent) {
     const { active, over } = event;
 
@@ -25,11 +49,13 @@ export function Board() {
 
       dispatch(moveCard({ cardID, newListID }));
     }
+
+    setActiveCard(null);
   }
 
   return (
     // START BOARD
-    <DndContext onDragEnd={handleDragEnd}>
+    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
       <div className="m-auto h-screen w-screen overflow-x-scroll text-center">
         <div className="flex h-full space-x-4">
           {lists.map((list) => (
@@ -37,6 +63,16 @@ export function Board() {
           ))}
         </div>
       </div>
+
+      <DragOverlay>
+        {activeCard ? (
+          <Card
+            title={activeCard.title}
+            description={activeCard.description}
+            cardID={activeCard.id}
+          />
+        ) : null}
+      </DragOverlay>
     </DndContext>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,20 +11,28 @@ type CardProps = {
   title: string;
   description: string;
   cardID: string;
+  transform?: { x: number; y: number };
 };
 
 // Returns a Card Component
-export function Card({ title, description, cardID }: CardProps) {
+export function Card({ title, description, cardID, transform }: CardProps) {
   // Drag and Drop
   const { attributes, listeners, setNodeRef } = useDraggable({
-    id: cardID,
+    id: `${cardID}`,
   });
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
 
   return (
     <div
       ref={setNodeRef}
       {...listeners}
       {...attributes}
+      style={style}
       className="card group/card m-3 flex min-h-24 w-full flex-col items-start rounded bg-off-white-light px-4 py-2 text-blue"
     >
       <h5 className="my-2 flex w-full items-end justify-between text-xl font-black">
